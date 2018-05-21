eb96127e0831dbd5df2c586f6b020ed55242671e
Lcharvol <charvolin0@gmail.com>
2018-05-21 17:39:56 +0200

    Fix degree 0

---
+++ src/solver/degrees.js
@@ -0,0 +1,53 @@
+import {
+    equals,
+    prop,
+    find,
+    propEq,
+} from 'ramda';
+
+import { printPolynomialDegree } from '../print/solution';
+import { FgGreen, FgRed } from '../constants/colors';
+import print from '../print';
+
+export const solverZeroDegree = dataStruct => {
+    const { reducedEquation, polynomialDegree } = dataStruct;
+    const a = prop('a', find(propEq('p', 0), reducedEquation));
+    printPolynomialDegree(polynomialDegree);
+    if (equals(a, 0)) {
+        print('All real number are the solution.', FgGreen);
+    } else {
+        print('Their is no solution.', FgRed);
+    }
+};
+
+export const solveFirstDegree = dataStruct => {
+    const { reducedEquation, polynomialDegree } = dataStruct;
+    const a = prop('a', find(propEq('p', 1), reducedEquation));
+    const b = prop('a', find(propEq('p', 0), reducedEquation));
+    const res = -b/a;
+    printPolynomialDegree(polynomialDegree);
+    print('The solution is: ', FgGreen)
+    print(res, FgGreen);
+};
+
+export const solveSecondDegree = dataStruct => {
+    const { reducedEquation, discriminant } = dataStruct;
+    let res1 = 0;
+    let res2 = 0;
+    const a = prop('a', find(propEq('p', 2), reducedEquation));
+    const b = prop('a', find(propEq('p', 1), reducedEquation));
+    if(discriminant > 0) {
+        const r = Math.sqrt(discriminant);
+        res1 = (-b + r) / (2 * a);
+        res2 = (-b - r) / (2 * a);
+        print('Discriminant is strictly positive, the two solutions are:', FgGreen);
+        print(res1, FgGreen);
+        print(res2, FgGreen);
+    } else if(discriminant === 0) {
+        res1 = -b / (2 * a)
+        print('Discriminant is null, the only solution is:', FgGreen);
+        print(res1, FgGreen);
+    } else {
+        print('Discriminant is negative, their is no solution.', FgGreen);
+    };
+};
--- src/solver/index.js
+++ src/solver/index.js
@@ -1,58 +1,25 @@
 import {
     equals,
-    prop,
-    find,
-    propEq,
 } from 'ramda';
 import { printPolynomialDegree, printReducedEquation } from '../print/solution';
 import print from '../print';
-import { FgGreen } from '../constants/colors';
 import { maxDegreeExit } from '../exit';
-
-const solveFirstDegree = dataStruct => {
-    const { reducedEquation } = dataStruct;
-    const a = prop('a', find(propEq('p', 1), reducedEquation));
-    const b = prop('a', find(propEq('p', 0), reducedEquation));
-    const res = -b/a;
-    print('The solution is: ', FgGreen)
-    print(res, FgGreen);
-};
-
-const solveSecondDegree = dataStruct => {
-    const { reducedEquation, discriminant } = dataStruct;
-    let res1 = 0;
-    let res2 = 0;
-    const a = prop('a', find(propEq('p', 2), reducedEquation));
-    const b = prop('a', find(propEq('p', 1), reducedEquation));
-    if(discriminant > 0) {
-        const r = Math.sqrt(discriminant);
-        res1 = (-b + r) / (2 * a);
-        res2 = (-b - r) / (2 * a);
-        print('Discriminant is strictly positive, the two solutions are:', FgGreen);
-        print(res1, FgGreen);
-        print(res2, FgGreen);
-    } else if(discriminant === 0) {
-        res1 = -b / (2 * a)
-        print('Discriminant is null, the only solution is:', FgGreen);
-        print(res1, FgGreen);
-    } else {
-        print('Discriminant is negative, their is no solution.', FgGreen);
-    };
-};
+import {
+    solverZeroDegree,
+    solveFirstDegree,
+    solveSecondDegree,
+} from './degrees';
 
 const solve = dataStruct => {
     const { polynomialDegree } = dataStruct;
     printReducedEquation(dataStruct);
     if(equals(polynomialDegree, 0)) {
-        printPolynomialDegree(polynomialDegree);
-        print('All real number are the solution.', FgGreen);
+        solverZeroDegree(dataStruct);
     } else if (equals(polynomialDegree, 1)) {
-        printPolynomialDegree(polynomialDegree);
         solveFirstDegree(dataStruct);
     } else if (equals(polynomialDegree, 2)) {
         solveSecondDegree(dataStruct);
-    }
-    else {
+    } else {
         printPolynomialDegree(polynomialDegree);
         maxDegreeExit();
     }
